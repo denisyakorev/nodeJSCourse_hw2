6535e7cd339a3a85d40ea07c30944faca6fca01f
Denis Iakorev <denis_iakorev@epam.com>
2021-10-25 08:42:35 +0300

    move compile files into separate dir

---
+++ task1/dist/server.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var controllers_1 = require("./controllers");
+var express = require('express');
+var app = express();
+var port = 3000;
+app.use(express.json());
+app.get('/', controllers_1.homepage);
+app.delete('/user/:id', controllers_1.deleteUser);
+app.get('/user/:id', controllers_1.getUser);
+app.put('/user/:id', controllers_1.updateUser);
+app.get('/user', controllers_1.getAutoSuggestUsers);
+app.post('/user', controllers_1.addUser);
+app.listen(port, function () { return console.log("Server started on port " + port); });
--- task1/server.ts
+++ task1/server.ts
@@ -1,5 +1,5 @@
 import {Response} from "express";
-import {addUser, deleteUser, getAutoSuggestUsers, getUser, homepage, updateUser} from "./views";
+import {addUser, deleteUser, getAutoSuggestUsers, getUser, homepage, updateUser} from "./controllers";
 
 const express = require('express');
 const app = express();
@@ -18,7 +18,6 @@ app.put('/user/:id', updateUser);
 app.get('/user', getAutoSuggestUsers);
 
 app.post('/user', addUser);
-
 
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
